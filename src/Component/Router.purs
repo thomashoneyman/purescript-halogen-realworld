be0fd593b0f3b269871017d747461978b35a5f54
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-12-05 19:26:52 -0800

    Update routing

--- src/Component/Router.purs
+++ src/Component/Router.purs
@@ -9,11 +9,15 @@ import Capability.ManageResource (class ManageAuthResource, class ManageResource
 import Capability.Navigate (class Navigate)
 import Capability.Now (class Now)
 import Component.Page.Home as Home
+import Component.Page.Settings as Settings
 import Control.Monad.Reader (class MonadAsk)
+import Data.Either.Nested (Either2)
+import Data.Functor.Coproduct.Nested (Coproduct2)
 import Data.Maybe (Maybe(..))
 import Data.Route (Route(..))
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
+import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
 
 type State =
@@ -22,6 +26,14 @@ type State =
 
 data Query a
   = Navigate Route a
+
+type ChildQuery = Coproduct2
+  Home.Query
+  Settings.Query
+
+type ChildSlot = Either2
+  Unit
+  Unit
 
 component
   :: forall m
@@ -44,19 +56,19 @@ component =
 
   where 
 
-  render :: State -> H.ParentHTML Query Home.Query Unit m
+  render :: State -> H.ParentHTML Query ChildQuery ChildSlot m
   render { route } = case route of
-    Home -> HH.slot unit Home.component unit (const Nothing)
+    Home -> HH.slot' CP.cp1 unit Home.component unit (const Nothing)
     Login -> HH.div_ []
     Register -> HH.div_ []
-    Settings -> HH.div_ []
+    Settings -> HH.slot' CP.cp2 unit Settings.component unit (const Nothing)
     Editor -> HH.div_ []
     EditArticle _ -> HH.div_ []
     ViewArticle _ -> HH.div_ []
     Profile _ -> HH.div_ []
     Favorites _ -> HH.div_ []
   
-  eval :: Query ~> H.ParentDSL State Query Home.Query Unit Void m
+  eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void m
   eval = case _ of
     Navigate dest a -> do
       { route } <- H.get 
