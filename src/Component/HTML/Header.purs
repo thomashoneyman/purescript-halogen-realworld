eb48523e58dd6fa9213c228c82aeb4945199d171
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-12-17 18:48:19 -0800

    build errors fixed

--- src/Component/HTML/Header.purs
+++ src/Component/HTML/Header.purs
@@ -3,10 +3,10 @@ module Conduit.Component.HTML.Header where
 import Prelude
 
 import Conduit.Api.Request (AuthUser)
-import Conduit.Component.HTML.Utils (css, guardHtml)
+import Conduit.Component.HTML.Utils (css, whenElem)
+import Conduit.Data.Route (Route(..))
 import Data.Maybe (Maybe, isNothing, isJust)
 import Data.Monoid (guard)
-import Conduit.Data.Route (Route(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
@@ -29,10 +29,14 @@ header authUser route navigate =
       , HH.ul
         [ css "nav navbar-nav pull-xs-right" ]
         [ navItem Home 
-        , navItem Editor # guardHtml (isJust authUser)
-        , navItem Settings # guardHtml (isJust authUser)
-        , navItem Login # guardHtml (isNothing authUser)
-        , navItem Register # guardHtml (isNothing authUser)
+        , whenElem (isJust authUser) \_ -> 
+            navItem Editor
+        , whenElem (isJust authUser) \_ -> 
+            navItem Settings
+        , whenElem (isNothing authUser) \_ ->
+            navItem Login
+        , whenElem (isNothing authUser) \_ ->
+            navItem Register
         ]
       ]
     ]
