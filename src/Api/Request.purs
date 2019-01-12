d650c069b46201eabe264c2d6a60262332299ab8
Thomas R. Honeyman <admin@thomashoneyman.com>
2019-01-11 18:55:56 -0800

    Modifications to the request module

--- src/Api/Request.purs
+++ src/Api/Request.purs
@@ -47,60 +47,73 @@ import Web.HTML (window)
 import Web.HTML.Window (localStorage)
 import Web.Storage.Storage (getItem, removeItem, setItem)
 
--- | As usual, we'll start with data types we'll leverage throughout the rest of this module. Let's
--- | start with a token, which will be used to authenticate requests to our server. This type is 
--- | necessary for requests, but we don't want to be able to inadvertently construct an invalid
--- | token, read its contents, or otherwise manipulate the string. We can achieve this using the
--- | smart constructor pattern: we'll create a new type called `Token` and we won't export its 
--- | constructor. The only functions that will be able to manipulate this value exist in this 
--- | module.
--- | 
--- | The only way to actually get one of these tokens is to use a request from this module (like 
--- | `login` or `register`). For that reason, any time we see this type used outside this module,
--- | we're guaranteed that it was retrieved properly before use.
+-- | As usual, we'll start with types we'll use throughout this module. 
+
+-- | Conduit uses a REST API and secures certain endpoints using a JSON Web Token (JWT). That means
+-- | most requests will use the token to attach an "Authorization" header. It's critical that this
+-- | token is not inadvertently exposed and that we attach a correct token to requests. We can 
+-- | prevent the token from being changed and restrict the ways it can be created using the smart
+-- | constructor pattern. To learn more about this pattern, see:
+-- |
+-- | https://thomashoneyman.com/guides/real-world-halogen/design-data-pure-functions/#restricting-the-domain-using-smart-constructors
+-- |
+-- | To achieve this, we'll create a newtype without exporting its constructor. That means that
+-- | the only functions that will be able to access the string within the `Token` type are in 
+-- | this module (like `login` and `register`). We can be confident that a `Token` used outside
+-- | this module was retrieved properly and hasn't been tampered with in the meantime.
 newtype Token = Token String
 
--- | Notably, we'll skip the usual `newtype` instance so that we can't sneakily use the `wrap` or 
--- | `unwrap` functions to create a `Token`.
+-- | No `newtype` instance allowed! That would allow us to use the `wrap` and `unwrap` functions to
+-- | access the string within the `Token` constructor.
 derive instance eqToken :: Eq Token
 derive instance ordToken :: Ord Token
 
--- | We won't derive a `Show` instance because we don't ever want to reveal the token. Instead, we'll
--- | provide a manual instance.
+-- | We won't derive a `Show` instance, either, because we don't ever want to reveal the token. 
+-- | Instead, we'll provide a manual instance.
 instance showToken :: Show Token where
   show (Token _) = "Token {- token -}"
 
 -- | Next, we'll give a meaningful type to the string that represents the base or root url of the 
--- | application. In my opinion, we don't need to be strict about how this string is constructed,
--- | because we hard-code it once in `Main` and that's it.
+-- | application. In my opinion, we don't need to be nearly so strict about how this string is 
+-- | constructed, because we hard-code it once in `Main` and never touch it again. We could 
+-- | reasonably have used the smart constructor pattern, though.
 newtype BaseURL = BaseURL String
 
 -- | Next, we'll create a type to represent the different methods our API supports. An API request
 -- | can be GET, POST, PUT, or DELETE. These methods are already captured by the `Data.HTTP.Method`
 -- | module, but I've opted for a custom type because, in our API, only `Post` and `Put` can have
--- | optional JSON bodies. Let's keep our invalid states unrepresentable by preventing a `Get`
--- | request with a JSON body.
+-- | optional JSON bodies. There should never be a `Get` request with a JSON body. This type 
+-- | prevents that from occurring.
 data RequestMethod 
   = Get
   | Post (Maybe Json)
   | Put (Maybe Json)
   | Delete
 
--- | The BaseURL and token are incidental details in making a request. They are necessary, but not
--- | relevant to our business logic. This type, however, captures the two most vital pieces of 
--- | information necessary to make a request: where and how?
+-- | The base url and token are necessary to make a request, but are low-level implementation
+-- | details.When actually using the API, we generally worry about which endpoint we'd like to 
+-- | access and what request method we'd like to use.
 -- |
--- | The `Endpoint` type is a type-safe representation of the possible endpoints in our API. And we 
--- | defined the `RequestMethod` above.
+-- | The `RequestOptions` type below relies on the `Endpoint` data type (which captures the possible
+-- | API locations we can access) and the `RequestMethod` type we just defined (which describes how
+-- | we access those endpoints). 
+-- |
+-- | As a brief aside: even that information is too low-level for our business logic! In the 
+-- | rest of the application we'll use functions that work on the resources we're managing without 
+-- | caring about the implementation (REST, RPC, local files, mocks, etc.). This type will be
+-- | used to write a REST implementation for our resource management capabilities. 
+-- |
+-- | Check out the `Conduit.Capability.Resource.*` modules for examples of the high-level functions
+-- | we'll use in our business logic.
 type RequestOptions =
   { endpoint :: Endpoint
   , method :: RequestMethod
   }
 
--- | Finally, we can use our BaseURL, token, and request options to create a data structure
--- | that the `Affjax` library understands and can use to make asynchronous requests. We won't
--- | use this function too often directly; it's instead used as a helper in all the request 
--- | functions defined below and in the `Conduit.Api.Utils` modules.
+-- | Let's bring these `BaseURL`, `Token`, and `RequestOptions` types together to create a data 
+-- | structure that the `Affjax` library understands and can use to make asynchronous requests. 
+-- | We won't use this function too often directly; it's instead used as a helper in all the 
+-- | request functions defined below and in the `Conduit.Api.Utils` modules.
 defaultRequest :: BaseURL -> Maybe Token -> RequestOptions -> Request Json
 defaultRequest (BaseURL baseUrl) auth { endpoint, method } =
   { method: Left method 
@@ -122,9 +135,10 @@ defaultRequest (BaseURL baseUrl) auth { endpoint, method } =
     Delete -> Tuple DELETE Nothing
 
 -- | The following data types and functions aren't a natural fit for this module, but they've 
--- | been included here because they operate on tokens. We don't want to allow any reading or
--- | manipulation of auth tokens outside this module, so we'll somewhat awkwardly define 
--- | registration and login requests in this module and export them.
+-- | been included here because they operate on tokens. Remember: we can't create or manipulate 
+-- | the `Token` type outside this module, so we'll somewhat awkwardly define the registration
+-- | and login requests in this module. These requests will be the only way to create an auth
+-- | token in the system.
 
 -- | First, we'll define a few data types as the inputs for our login and authentication requests.
 -- |
