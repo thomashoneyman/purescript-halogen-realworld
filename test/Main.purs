b964e6c2d6967797a65ba2a2f53b5a1fb615dfca
Thomas R. Honeyman <admin@thomashoneyman.com>
2019-01-13 17:24:42 -0800

    add note to Test/

--- test/Main.purs
+++ test/Main.purs
@@ -1,3 +1,6 @@
+-- | While I would love to demonstrate how to test Halogen applications, I quite simply haven't
+-- | had the time! If you would like to see tests for a particular part of the app, please consider
+-- | filing an issue or participating in an existing one.
 module Test.Main where
 
 import Prelude
