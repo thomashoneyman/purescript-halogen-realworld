thomashoneyman/purescript-halogen-realworld

commit e56fc522373d4c6b5a41fe03b403a792472ab52f

Add pagination & rework capabilities (#12)
* wip: rework capabilities

* Finish api rework

* minor fixes

* properly hide requests

* add pagination

src/AppM.purs
@@ -5,34 +5,34 @@ module Conduit.AppM where
 
 import Prelude
 
-import Conduit.Api.Endpoint (Endpoint(..), noArticleParams)
-import Conduit.Api.Request (AuthType(..), BaseURL, delete, get, post, put, runRequest)
+import Conduit.Api.Request (BaseURL, RequestMethod(..))
 import Conduit.Api.Request as Request
-import Conduit.Api.Utils (withAuthUser, withAuthUser_, withUser)
-import Conduit.Capability.Authenticate (class Authenticate, writeAuth)
-import Conduit.Capability.LogMessages (class LogMessages, logError)
-import Conduit.Capability.ManageResource (class ManageAuthResource, class ManageResource)
+import Conduit.Api.Utils (authenticate, decode, decodeAt, decodeWithUser, mkAuthRequest, mkRequest)
+import Conduit.Capability.LogMessages (class LogMessages)
 import Conduit.Capability.Navigate (class Navigate, navigate)
 import Conduit.Capability.Now (class Now)
-import Conduit.Data.Article (decodeArticleWithMetadata, decodeArticles)
-import Conduit.Data.Author (decodeAuthorProfile)
-import Conduit.Data.Comment (decodeComment, decodeComments)
+import Conduit.Capability.Resource.Article (class ManageArticle)
+import Conduit.Capability.Resource.Comment (class ManageComment)
+import Conduit.Capability.Resource.Tag (class ManageTag)
+import Conduit.Capability.Resource.User (class ManageUser)
+import Conduit.Data.Article (decodeArticle, decodeArticles)
+import Conduit.Data.Author (decodeAuthor)
+import Conduit.Data.Comment (decodeComments)
+import Conduit.Data.Endpoint (Endpoint(..), noArticleParams)
 import Conduit.Data.Log (LogType(..))
 import Conduit.Data.Log as Log
-import Conduit.Data.Profile (decodeProfileWithEmail, encodeUpdateProfile)
+import Conduit.Data.Profile (Profile)
 import Conduit.Data.Route as Route
 import Control.Monad.Reader.Trans (class MonadAsk, ReaderT, ask, asks, runReaderT)
-import Data.Argonaut.Decode (decodeJson, (.:))
 import Data.Argonaut.Encode (encodeJson)
-import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
-import Data.Tuple (Tuple(..))
 import Effect.Aff (Aff)
-import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Console as Console
 import Effect.Now as Now
+import Effect.Ref (Ref)
 import Routing.Duplex (print)
 import Routing.Hash (setHash)
 import Type.Equality (class TypeEquals, from)
@@ -43,6 +43,7 @@ import Type.Equality (class TypeEquals, from)
 type Env = 
   { logLevel :: LogLevel 
   , baseUrl :: BaseURL
+  , currentUser :: Ref (Maybe Profile)
   }
 
 data LogLevel = Dev | Prod
@@ -105,86 +106,98 @@ instance logMessagesAppM :: LogMessages AppM where
       Prod, Debug -> pure unit
       _, _ -> Console.log $ Log.message l
 
--- We'll use local storage to load, save, and destroy credentials. In our tests,
--- we'll hardcode a particular user we have test data about in our system.
-
-instance authenticateAppM :: Authenticate AppM where
-  authenticate fields = do 
-    { baseUrl } <- ask
-    liftAff (Request.login fields baseUrl) >>= case _ of
-      Left err -> logError err *> pure (Left err)
-      Right (Tuple au prof) -> writeAuth au *> pure (Right prof)
-  readAuth = liftEffect Request.readAuthUserFromLocalStorage
-  writeAuth = liftEffect <<< Request.writeAuthUserToLocalStorage
-  deleteAuth = liftEffect Request.deleteAuthUserFromLocalStorage
-  
 -- The root of our application is watching for hash changes, so to route from 
 -- location to location we just need to set the hash. Logging out is more
 -- involved; we need to invalidate the session.
 
 instance navigateAppM :: Navigate AppM where
-  navigate = liftEffect <<< setHash <<< print Route.routeCodec 
+  navigate = 
+    liftEffect <<< setHash <<< print Route.routeCodec 
+
   logout = do
-    liftEffect Request.deleteAuthUserFromLocalStorage 
+    liftEffect Request.removeToken 
     navigate Route.Home
 
--- Our first resource class describes resources that do not require any authentication.
+-- Our first resource class describes what operations we have available to manage users.
+-- Logging in and registration require manipulating a token, so those are imported. All
+-- other requests rely on our `mkRequest` or `mkAuthRequest` helpers.
+instance manageUserAppM :: ManageUser AppM where
+  loginUser = 
+    authenticate Request.login
 
-instance manageResourceAppM :: ManageResource AppM where
-  register body = do
-    { baseUrl } <- ask
-    liftAff (Request.register body baseUrl) >>= case _ of
-      Left err -> logError err *> pure (Left err)
-      Right (Tuple au prof) -> writeAuth au *> pure (Right prof) 
-  getTags = do
-    let tagDecoder = (_ .: "tags") <=< decodeJson
-    { baseUrl } <- ask
-    runRequest tagDecoder $ get NoAuth Tags baseUrl
-  getAuthor u = 
-    withUser decodeAuthorProfile $ get NoAuth $ Profiles u
-  getComments u = 
-    withUser decodeComments $ get NoAuth $ Comments u
+  registerUser = 
+    authenticate Request.register
+
+  getCurrentUser = 
+    mkAuthRequest { endpoint: User, method: Get }
+      >>= decode (decodeAt "user")
+
+  getAuthor username = 
+    mkRequest { endpoint: Profiles username, method: Get }
+      >>= decodeWithUser (\mbU -> decodeAuthor mbU <=< decodeAt "profile")
+
+  updateUser fields = 
+    void $ mkAuthRequest { endpoint: User, method: Post (Just (encodeJson fields)) }
+
+  followUser username = 
+    mkAuthRequest { endpoint: Follow username, method: Post Nothing }
+      >>= decodeWithUser (\mbU -> decodeAuthor mbU <=< decodeAt "profile")
+  
+  unfollowUser username =
+    mkAuthRequest { endpoint: Follow username, method: Delete }
+      >>= decodeWithUser (\mbU -> decodeAuthor mbU <=< decodeAt "profile")
+
+
+-- Our operations for managing tags
+instance manageTagAppM :: ManageTag AppM where 
+  getAllTags = 
+    mkRequest { endpoint: Tags, method: Get }
+      >>= decode (decodeAt "tags")
+
+-- Our operations for managing comments
+instance manageCommentAppm :: ManageComment AppM where
+  getComments slug =
+    mkRequest { endpoint: Comments slug, method: Get }
+      >>= decodeWithUser decodeComments
+  
+  createComment slug fields = 
+    let method = Post $ Just $ encodeJson fields
+     in void $ mkAuthRequest { endpoint: Comments slug, method }
+
+  deleteComment slug id =
+    void $ mkAuthRequest { endpoint: Comment slug id, method: Delete }
+
+-- Our operations for managing articles 
+instance manageArticleAppM :: ManageArticle AppM where
   getArticle slug =
-    withUser decodeArticleWithMetadata $ get NoAuth $ Article slug
-  getArticles params = 
-    withUser decodeArticles $ get NoAuth $ Articles params
+    mkRequest { endpoint: Article slug, method: Get }
+      >>= decodeWithUser decodeArticle
+  
+  getArticles fields =
+    mkRequest { endpoint: Articles fields, method: Get }
+      >>= decodeWithUser decodeArticles
+  
+  createArticle article = do
+    let method = Post $ Just $ encodeJson { article }
+    mkAuthRequest { endpoint: Articles noArticleParams, method }
+      >>= decodeWithUser decodeArticle
+  
+  updateArticle slug article = do
+    let method = Post $ Just $ encodeJson { article }
+    mkAuthRequest { endpoint: Article slug, method }
+      >>= decodeWithUser decodeArticle
 
--- Our second resource class describes resources that do require authentication.
+  deleteArticle slug = 
+    void $ mkAuthRequest { endpoint: Article slug, method: Delete }
+  
+  favoriteArticle slug = 
+    mkAuthRequest { endpoint: Favorite slug, method: Post Nothing }
+      >>= decodeWithUser decodeArticle
 
-instance manageAuthResourceAppM :: ManageAuthResource AppM where
-  getUser = 
-    withAuthUser (const decodeProfileWithEmail) \t -> 
-      get (Auth t) User
-  updateUser p = 
-    withAuthUser_ \t -> 
-      put (Auth t) ( encodeUpdateProfile p) User
-  followUser u = 
-    withAuthUser decodeAuthorProfile \t -> 
-      post (Auth t) Nothing (Follow u)
-  unfollowUser u = 
-    withAuthUser decodeAuthorProfile \t -> 
-      delete (Auth t) (Follow u)
-  createArticle article = 
-    withAuthUser decodeArticleWithMetadata \t -> 
-      post (Auth t) (Just $ encodeJson { article }) (Articles noArticleParams)
-  updateArticle s article = 
-    withAuthUser decodeArticleWithMetadata \t -> 
-      put (Auth t) (encodeJson { article }) (Article s)
-  deleteArticle s = 
-    withAuthUser_ \t -> 
-      delete (Auth t) (Article s)
-  createComment s comment = 
-    withAuthUser (\x -> decodeComment x <=< (_ .: "comment") <=< decodeJson) \t -> 
-      post (Auth t) (Just $ encodeJson { comment }) (Comments s)
-  deleteComment s cid = 
-    withAuthUser_ \t -> 
-      delete (Auth t) (Comment s cid)
-  favoriteArticle s = 
-    withAuthUser decodeArticleWithMetadata \t -> 
-      post (Auth t) Nothing (Favorite s)
-  unfavoriteArticle s = 
-    withAuthUser decodeArticleWithMetadata \t -> 
-      delete (Auth t) (Favorite s)
-  getFeed p = 
-    withAuthUser decodeArticles \t -> 
-      get (Auth t) (Feed p)
+  unfavoriteArticle slug = 
+    mkAuthRequest { endpoint: Favorite slug, method: Delete }
+      >>= decodeWithUser decodeArticle
+  
+  getCurrentUserFeed params =
+    mkAuthRequest { endpoint: Feed params, method: Get }
+      >>= decodeWithUser decodeArticles

src/Api/Request.purs
@@ -1,43 +1,39 @@
-module Conduit.Api.Request
-  ( AuthUser -- constructors not exported
-  , username
-  , authUserKey
-  , readAuthUserFromLocalStorage
-  , writeAuthUserToLocalStorage
-  , deleteAuthUserFromLocalStorage
-  , get
-  , post
-  , put
-  , delete
-  , AuthType(..)
+module Conduit.Api.Request 
+  ( Token -- constructor and decoders not exported
   , BaseURL(..)
-  , runRequest
-  , RegisterFields
-  , LoginFields
+  , RequestMethod(..)
+  , RequestOptions(..)
+  , defaultRequest
+  , Unlifted(..)
+  , RegisterFields(..)
+  , LoginFields(..)
+  , AuthFieldsRep(..)
   , login
   , register
+  , readToken
+  , writeToken
+  , removeToken
   ) where
 
 import Prelude
 
-import Affjax as AX
+import Affjax (Request, printResponseFormatError, request)
 import Affjax.RequestBody as RB
-import Affjax.RequestHeader as RH
+import Affjax.RequestHeader (RequestHeader(..))
 import Affjax.ResponseFormat as RF
-import Conduit.Api.Endpoint (Endpoint(..), endpointCodec)
-import Data.Argonaut.Core (Json, stringify)
+import Conduit.Data.Email (Email)
+import Conduit.Data.Endpoint (Endpoint(..), endpointCodec)
+import Conduit.Data.Profile (Profile)
+import Conduit.Data.Username (Username)
+import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (decodeJson, (.:))
 import Data.Argonaut.Encode (encodeJson)
-import Data.Argonaut.Parser (jsonParser)
 import Data.Bifunctor (lmap)
-import Data.Either (Either(..), note)
-import Conduit.Data.Email (Email)
+import Data.Either (Either(..))
 import Data.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype, unwrap)
-import Conduit.Data.Profile (Profile)
 import Data.Tuple (Tuple(..))
-import Conduit.Data.Username (Username)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Routing.Duplex (print)
@@ -45,72 +41,20 @@ import Web.HTML (window)
 import Web.HTML.Window (localStorage)
 import Web.Storage.Storage (getItem, removeItem, setItem)
 
--- We need a token in order to authenticate requests in the application. However,
--- we also want to ensure that this value never gets constructed except via logging
--- in, registering, or reading from local storage. We also need to be able to 
--- retrieve the username of the currently logged-in user at any point. We could
--- store that in global state, but for the sake of keeping related data together
--- and not having to invalidate / update in two places, we'll keep it in storage.
+-- Constructor hidden so that tokens can't be inadvertently read or arbitrarily created. 
+-- One downside of this is that any function that manipulates a token must be defined in 
+-- this module and exported, but the upside is that we always know the token was retrieved
+-- properly before use.
+newtype Token = Token String
 
-data AuthUser = AuthUser Username String
-
-derive instance eqAuthUser :: Eq AuthUser
-derive instance ordAuthUser :: Ord AuthUser
+derive instance eqToken :: Eq Token
+derive instance ordToken :: Ord Token
 
 -- We don't want to inadvertently show the user's token, so we'll write a manual
 -- `Show` instance
 
-instance showAuthUser :: Show AuthUser where
-  show (AuthUser uname _) = "AuthUser (" <> show uname <> ") {- token -}"
-
--- We've need to be able to retrieve the username, but not the token
-
-username :: AuthUser -> Username
-username (AuthUser uname _) = uname
-
--- We need to be able to serialize and de-serialize AuthUsers from JSON. We won't
--- export these functions, but they'll provide necessary parsing for our request
--- functions.
-
--- Do not export.
-decodeAuthUser :: Json -> Either String AuthUser
-decodeAuthUser json = do 
-  obj <- decodeJson json
-  uname <- obj .: "username"
-  tok <- obj .: "token"
-  pure $ AuthUser uname tok
-
--- Do not export.
-encodeAuthUser :: AuthUser -> Json
-encodeAuthUser (AuthUser uname t) = encodeJson { username: uname, token: t }
-
-authUserKey = "authUser" :: String
-
--- These functions can be exported; they construct or use an auth user and its
--- information but don't allow the end user to see anything internal, like the
--- specific token of a given user. Because these are used to implement our
--- Authenticate capability, our test monad can use different implementations like
--- dummy data instead.
-
-readAuthUserFromLocalStorage :: Effect (Either String AuthUser)
-readAuthUserFromLocalStorage = do
-  rec <- getItem authUserKey =<< localStorage =<< window
-  pure $ decodeAuthUser =<< jsonParser =<< note "Failed to retrieve token" rec
-
-writeAuthUserToLocalStorage :: AuthUser -> Effect Unit
-writeAuthUserToLocalStorage au =
-  setItem authUserKey (stringify $ encodeAuthUser au) =<< localStorage =<< window
-
-deleteAuthUserFromLocalStorage :: Effect Unit
-deleteAuthUserFromLocalStorage =
-  removeItem authUserKey =<< localStorage =<< window
-
--- We'll represent authenticated and non-authenticated requests with a custom type
--- to avoid ambiguity over what a `Nothing` value means.
-data AuthType = NoAuth | Auth AuthUser
-
-derive instance eqAuthType :: Eq AuthType
-derive instance ordAuthType :: Ord AuthType
+instance showToken :: Show Token where
+  show (Token _) = "Token {- token -}"
 
 -- We build a BaseURL newtype to have a meaningful type to pass around in parameters
 -- a more powerful type that does validation could be an improvement.
@@ -118,74 +62,94 @@ newtype BaseURL = BaseURL String
 
 derive instance newtypeBaseURL :: Newtype BaseURL _
 
--- We need to be able to make requests. Here, we'll assume that we've been provided
--- with an `AuthUser`. In practice, the only way to get an `AuthUser` is via our
--- `Authenticate` capability, so we'll leverage that to actually perform requests.
--- These data types create the request data type, but do not actually perform effects.
+data RequestMethod 
+  = Get
+  | Post (Maybe Json)
+  | Put (Maybe Json)
+  | Delete
 
-get :: AuthType -> Endpoint -> BaseURL -> AX.Request Json
-get auth = mkRequest GET auth Nothing
+type RequestOptions =
+  { endpoint :: Endpoint
+  , method :: RequestMethod
+  }
 
-post :: AuthType -> Maybe Json -> Endpoint -> BaseURL -> AX.Request Json
-post auth = mkRequest POST auth
-
-put :: AuthType -> Json -> Endpoint -> BaseURL -> AX.Request Json
-put auth body = mkRequest PUT auth (Just body)
-
-delete :: AuthType -> Endpoint -> BaseURL -> AX.Request Json
-delete auth = mkRequest DELETE auth Nothing
-
--- The underlying helper to construct a request 
-
-mkRequest :: Method -> AuthType -> Maybe Json -> Endpoint -> BaseURL -> AX.Request Json
-mkRequest method auth body endpoint baseUrl =
+defaultRequest :: BaseURL -> Maybe Token -> RequestOptions -> Request Json
+defaultRequest baseUrl auth { endpoint, method } =
   { method: Left method 
-  , url: unwrap baseUrl <> print endpointCodec endpoint  -- TODO: FIXME
+  , url: unwrap baseUrl <> print endpointCodec endpoint
   , headers: case auth of
-      NoAuth -> []
-      Auth (AuthUser _ t) -> [ RH.RequestHeader "Authorization" $ "Token " <> t ]
+      Nothing -> []
+      Just (Token t) -> [ RequestHeader "Authorization" $ "Token " <> t ]
   , content: RB.json <$> body
   , username: Nothing
   , password: Nothing
   , withCredentials: false
   , responseFormat: RF.json
   }
+  where
+  Tuple method body = case method of
+    Get -> Tuple GET Nothing
+    Post b -> Tuple POST b
+    Put b -> Tuple PUT b
+    Delete -> Tuple DELETE Nothing
 
--- Helper to actually run the request in Aff
+-- The `Unlifted` type here allows us to turn any concrete type of kind  `Type` 
+-- into a type with a single argument of kind `(Type -> Type)`. This is useful
+-- when we want to represent a value within various 'boxes'. For example, our
+-- we might like our `AuthFieldsRep` type to cover the possibilites that a password
+-- has a `Maybe` value `(Maybe String)`, an array value `(Array String)`, or a 
+-- simple `String` value `(Unlifted String)`.
+type Unlifted a = a 
 
-runRequest :: forall m a. MonadAff m => (Json -> Either String a) -> AX.Request Json -> m (Either String a)
-runRequest decode req = do
-  res <- liftAff (AX.request req)
-  pure (decode =<< lmap RF.printResponseFormatError res.body)
+-- For example, in both of these cases, we want to ensure a password was provided.
+-- However, in the Capability.Resource.User module we'll see an example of when this 
+-- should be a `Maybe` value instead.
+type RegisterFields = { | AuthFieldsRep Unlifted (username :: Username) }
+type LoginFields = { | AuthFieldsRep Unlifted () }
 
+-- The underlying shared row for various requests which manage user credentials.
+type AuthFieldsRep f r = ( email :: Email, password :: f String | r )
 
--- Fields necessary to authenticate a user. Could be done with extensible rows, but 
--- there aren't enough fields to justify it, IMO
+-- For decoding a user response from the server into an Token + Profile. Not exported
+-- in order to ensure that the only way to acquire a token is via our user management 
+-- functions.
+decodeAuthProfile :: Json -> Either String (Tuple Token Profile)
+decodeAuthProfile json = do
+  str <- decodeJson =<< (_ .: "token") =<< decodeJson json
+  prof <- decodeJson json
+  pure $ Tuple (Token str) prof
 
-type RegisterFields =
-  { username :: Username
-  , email :: Email
-  , password :: String
-  }
+-- Required to write in this module because it operates on tokens
+login :: forall m. MonadAff m => BaseURL -> LoginFields -> m (Either String (Tuple Token Profile))
+login baseUrl fields = 
+  let method = Post $ Just $ encodeJson { user: fields } 
+   in requestUser baseUrl { endpoint: Login, method }
 
-type LoginFields = 
-  { email :: Email
-  , password :: String 
-  }
+-- Required to write in this module because it operates on tokens
+register :: forall m. MonadAff m => BaseURL -> RegisterFields -> m (Either String (Tuple Token Profile))
+register baseUrl fields = 
+  let method = Post $ Just $ encodeJson { user: fields }
+   in requestUser baseUrl { endpoint: Users, method } 
 
--- For auth tokens specifically, because the decoder is not exposed
+-- Same underlying mechanism for both requests
+requestUser :: forall m. MonadAff m => BaseURL -> RequestOptions -> m (Either String (Tuple Token Profile))
+requestUser baseUrl opts = do
+  res <- liftAff $ request $ defaultRequest baseUrl Nothing opts
+  pure $ decodeAuthProfile =<< (_ .: "user") =<< decodeJson =<< lmap printResponseFormatError res.body
 
-login :: forall m. MonadAff m => LoginFields -> BaseURL -> m (Either String (Tuple AuthUser Profile))
-login body = runRequest decodeUser <<< post NoAuth (Just $ encodeJson { user: body }) Login
+-- Managing tokens in local storage
 
-register :: forall m. MonadAff m => RegisterFields -> BaseURL -> m (Either String (Tuple AuthUser Profile))
-register body = runRequest decodeUser <<< post NoAuth (Just $ encodeJson { user: body }) Users
+tokenKey = "token" :: String
 
--- For decoding a user response from the server into an AuthUser + Profile
+readToken :: Effect (Maybe Token)
+readToken = do
+  str <- getItem tokenKey =<< localStorage =<< window
+  pure $ map Token str
 
-decodeUser :: Json -> Either String (Tuple AuthUser Profile)
-decodeUser json = do
-  json' <- (_ .: "user") =<< decodeJson json
-  au <- decodeAuthUser json'
-  prof <- decodeJson json'
-  pure $ Tuple au prof
+writeToken :: Token -> Effect Unit
+writeToken (Token str) =
+  setItem tokenKey str =<< localStorage =<< window
+
+removeToken :: Effect Unit
+removeToken =
+  removeItem tokenKey =<< localStorage =<< window

src/Api/Utils.purs
@@ -1,73 +1,90 @@
-module Conduit.Api.Utils
-  ( withUser
-  , withAuthUser
-  , withAuthUser_
-  ) where
+module Conduit.Api.Utils where
 
 import Prelude
 
-import Affjax (Request)
-import Conduit.Api.Request (AuthUser, BaseURL, runRequest, username)
-import Conduit.Capability.Authenticate (class Authenticate, readAuth)
+import Affjax (request)
+import Conduit.Api.Request (BaseURL, RequestOptions, Token, defaultRequest, readToken, writeToken)
 import Conduit.Capability.LogMessages (class LogMessages, logError)
-import Conduit.Capability.Navigate (class Navigate, logout)
-import Control.Monad.Reader (class MonadAsk, ask)
-import Data.Argonaut.Core (Json)
-import Data.Bifoldable (bitraverse_)
-import Data.Bitraversable (ltraverse)
-import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Conduit.Data.Profile (Profile)
 import Conduit.Data.Username (Username)
-import Effect.Aff.Class (class MonadAff)
+import Control.Monad.Reader (class MonadAsk, ask, asks)
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.:))
+import Data.Either (Either(..), hush)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Ref (Ref)
+import Effect.Ref as Ref
 
--- Helper functions that leverages several of our capabilities together to help
--- run requests that require authentication.
+-- Decode JSON within a particular key. For example: decode a Profile object
+-- within a larger object containing a "user" field with:
+--
+-- decodeProfile :: Json -> Either String Profile
+-- decodeProfile = decodeAt "user"
+decodeAt :: forall a. DecodeJson a => String -> Json -> Either String a
+decodeAt key = decodeJson <=< (_ .: key) <=< decodeJson
 
-withUser
-  :: forall m e a
-   . MonadAff m 
-  => LogMessages m 
-  => Navigate m 
-  => MonadAsk { baseUrl :: BaseURL | e } m
-  => Authenticate m
-  => (Maybe Username -> Json -> Either String a)
-  -> (BaseURL -> Request Json)
-  -> m (Either String a)
-withUser decode req = do
+-- Perform a request that does not require authentication
+mkRequest 
+  :: forall m r
+   . MonadAff m
+  => MonadAsk { baseUrl :: BaseURL | r } m
+  => RequestOptions
+  -> m (Maybe Json)
+mkRequest opts = do
   { baseUrl } <- ask
-  readAuth >>= case _ of
-    Left _ -> runRequest (decode Nothing) $ req baseUrl
-    Right au -> runRequest (decode $ Just $ username au) $ req baseUrl
+  res <- liftAff $ request $ defaultRequest baseUrl Nothing opts
+  pure $ hush res.body
 
-withAuthUser 
-  :: forall m a e
-   . MonadAff m 
+-- Perform a request that requires authentication
+mkAuthRequest 
+  :: forall m r
+   . MonadAff m
+  => MonadAsk { baseUrl :: BaseURL | r } m
+  => RequestOptions
+  -> m (Maybe Json)
+mkAuthRequest opts = do
+  { baseUrl } <- ask
+  token <- liftEffect readToken
+  res <- liftAff $ request $ defaultRequest baseUrl token opts
+  pure $ hush res.body
+
+-- Shared behaviors for logging in or registering
+authenticate 
+  :: forall m a r
+   . MonadAff m
+  => MonadAsk { baseUrl :: BaseURL, currentUser :: Ref (Maybe Profile) | r } m
   => LogMessages m
-  => Navigate m 
-  => MonadAsk { baseUrl :: BaseURL | e } m
-  => Authenticate m 
-  => (Maybe Username -> Json -> Either String a)
-  -> (AuthUser -> BaseURL -> Request Json)
-  -> m (Either String a)
-withAuthUser decode req =
-  readAuth >>= case _ of
-    Left err -> logError err *> pure (Left err) 
-    Right au -> do
-      { baseUrl } <- ask
-      res <- runRequest (decode $ Just $ username au) (req au baseUrl)
-      void $ ltraverse (\e -> logError e *> logout) res
-      pure res
+  => (BaseURL -> a -> m (Either String (Tuple Token Profile))) 
+  -> a 
+  -> m (Maybe Profile)
+authenticate req fields = do 
+  { baseUrl, currentUser } <- ask
+  req baseUrl fields >>= case _ of
+    Left err -> logError err *> pure Nothing
+    Right (Tuple token profile) -> do 
+      liftEffect $ writeToken token 
+      liftEffect $ Ref.write (Just profile) currentUser
+      pure (Just profile)
 
-withAuthUser_ 
-  :: forall m e
-   . MonadAff m 
+-- A small utility to log out decoding failures
+decode :: forall m a. LogMessages m => (Json -> Either String a) -> Maybe Json -> m (Maybe a)
+decode _ Nothing = logError "Response malformed" *> pure Nothing 
+decode decoder (Just json) = case decoder json of
+  Left err -> logError err *> pure Nothing
+  Right res -> pure (Just res)
+
+-- A small utility to help with decoders that require the current user as an argument
+decodeWithUser 
+  :: forall m a r
+   . MonadEffect m
+  => MonadAsk { currentUser :: Ref (Maybe Profile) | r } m
   => LogMessages m 
-  => Navigate m 
-  => MonadAsk { baseUrl :: BaseURL | e } m
-  => Authenticate m 
-  => (AuthUser -> BaseURL -> Request Json) 
-  -> m Unit 
-withAuthUser_ req =
-  readAuth >>= bitraverse_
-    (\e -> logError e *> logout)
-    (\au -> ask >>= runRequest pure <<< req au <<< _.baseUrl)
+  => (Maybe Username -> Json -> Either String a) 
+  -> Maybe Json 
+  -> m (Maybe a)
+decodeWithUser decoder json = do
+  mbProfile <- (liftEffect <<< Ref.read) =<< asks _.currentUser
+  decode (decoder (_.username <$> mbProfile)) json

src/Capability/Authenticate.purs
@@ -1,28 +1,0 @@
--- | This module describes the capability to read, write, and clear credentials 
--- | stored about a single user in the application. It is used for local storage 
--- | now, but could be switched to another source later.
-
-module Conduit.Capability.Authenticate where
-
-import Prelude
-
-import Conduit.Api.Request (AuthUser, LoginFields)
-import Control.Monad.Trans.Class (lift)
-import Data.Either (Either)
-import Conduit.Data.Profile (Profile)
-import Halogen (HalogenM)
-
--- The ability to read, write, and clear credentials. Includes an instance for 
--- HalogenM to make this class convenient to use in Halogen components.
-
-class Monad m <= Authenticate m where
-  authenticate :: LoginFields -> m (Either String Profile)
-  readAuth :: m (Either String AuthUser)
-  writeAuth :: AuthUser -> m Unit
-  deleteAuth :: m Unit
-
-instance authenticateHalogenM :: Authenticate m => Authenticate (HalogenM s f g p o m) where
-  authenticate = lift <<< authenticate
-  readAuth = lift readAuth
-  writeAuth = lift <<< writeAuth
-  deleteAuth = lift deleteAuth

commit cd1e97aece7d250d4f3c28c5f4e6c0a154a73d11

change method used in update requests (#44)
fixes regression introduced in #12

src/AppM.purs
@@ -211,7 +211,7 @@ instance manageUserAppM :: ManageUser AppM where
       >>= decodeWithUser decodeProfileAuthor
 
   updateUser fields =
-    void $ mkAuthRequest { endpoint: User, method: Post (Just (encodeJson fields)) }
+    void $ mkAuthRequest { endpoint: User, method: Put (Just (encodeJson fields)) }
 
   followUser username =
     mkAuthRequest { endpoint: Follow username, method: Post Nothing }
@@ -256,7 +256,7 @@ instance manageArticleAppM :: ManageArticle AppM where
       >>= decodeWithUser decodeArticle
 
   updateArticle slug article = do
-    let method = Post $ Just $ encodeJson { article }
+    let method = Put $ Just $ encodeJson { article }
     mkAuthRequest { endpoint: Article slug, method }
       >>= decodeWithUser decodeArticle
 