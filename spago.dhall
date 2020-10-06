0f41cf1b7fc2c704d56c4bf8a9729acdfe994658
Thomas Honeyman <admin@thomashoneyman.com>
2020-10-06 12:34:20 -0700

    Remove Unlifted section for unnecessary complexity

--- spago.dhall
+++ spago.dhall
@@ -1,31 +1,29 @@
-{ sources =
-    [ "src/**/*.purs", "test/**/*.purs" ]
-, name =
-    "purescript-halogen-realworld"
-, packages =
-    ./packages.dhall
+{ sources = [ "src/**/*.purs", "test/**/*.purs" ]
+, name = "purescript-halogen-realworld"
+, packages = ./packages.dhall
 , dependencies =
-    [ "prelude"
-    , "console"
-    , "effect"
-    , "variant"
-    , "nonempty"
-    , "aff"
-    , "halogen"
-    , "halogen-formless"
-    , "remotedata"
-    , "routing"
-    , "formatters"
-    , "routing-duplex"
-    , "now"
-    , "affjax"
-    , "slug"
-    , "precise-datetime"
-    , "typelevel-prelude"
-    , "argonaut-core"
-    , "argonaut-codecs"
-    , "aff-bus"
-    , "struct"
-    , "tolerant-argonaut"
-    ]
+  [ "aff"
+  , "aff-bus"
+  , "affjax"
+  , "argonaut-codecs"
+  , "argonaut-core"
+  , "codec-argonaut"
+  , "console"
+  , "effect"
+  , "formatters"
+  , "halogen"
+  , "halogen-formless"
+  , "nonempty"
+  , "now"
+  , "precise-datetime"
+  , "prelude"
+  , "remotedata"
+  , "routing"
+  , "routing-duplex"
+  , "slug"
+  , "struct"
+  , "tolerant-argonaut"
+  , "typelevel-prelude"
+  , "variant"
+  ]
 }
--- src/Api/Request.purs
+++ src/Api/Request.purs
@@ -11,10 +11,8 @@ module Conduit.Api.Request
   , RequestMethod(..)
   , RequestOptions(..)
   , defaultRequest
-  , Unlifted(..)
   , RegisterFields(..)
   , LoginFields(..)
-  , AuthFieldsRep(..)
   , login
   , register
   , readToken
@@ -142,98 +140,16 @@ defaultRequest (BaseURL baseUrl) auth { endpoint, method } =
 -- | and login requests in this module. These requests will be the only way to create an auth
 -- | token in the system.
 
--- | First, we'll define a few types as the inputs for our login and authentication requests.
--- |
--- | Both functions -- as well as functions from the `Conduit.Capability.Resource.User` module --
--- | share most of their required input fields. Rather than define three separate record types
--- | (which could fall out of sync with one another and introduce unnecessary boilerplate), we'll
--- | use PureScript's lovely extensible row types to share as much as possible among the inputs.
--- |
--- | This is possible because rows are a type-level list of keys and values, and these lists
--- | can be merged. Here's an example:
--- |
--- | ```purescript
--- | -- A row containing a label, "a" with a value of type `Int`, which can be extended with
--- | -- another row
--- | type RowA r = (a :: Int | r)
--- |
--- | -- A row containing all labels from `RowA`, plus an additional label "b" with a value of
--- | type `Int`, which can be extended by yet another row. The two versions below are equivalent.
--- | type RowAB r = (b :: Int | RowA r)
--- | type RowAB r = (a :: Int, b :: Int | r)
--- |
--- | -- A "closed" row, which simply means it cannot be extended with further rows.
--- | type RowABC = (c :: Int | RowA RowB)
--- |
--- | -- Records are really just rows under the hood, with nicer syntax
--- | type RecordABC = Record RowABC
--- | type RecordABC = { | RowABC }
--- | type RecordABC = { a :: Int, b :: Int, c :: Int }
--- | ```
+type RegisterFields =
+  { email :: Email
+  , password :: String
+  , username :: Username
+  }
 
--- | We're almost ready to get started, but we have a snag! We're going to define two record types
--- | using most of the same underlying row. But in some records, we'll have a password field with
--- | the type `Maybe String`, but in others, we'll have a password field with the type `String`.
--- | How can we still share these rows?
--- |
--- | We can reconcile this by defining our row so that the password field is in a "box" represented
--- | by a type variable, `box`. This box can be filled in with various types of the kind
--- | `Type -> Type`, like `Maybe a`, `Array a`, `List a`, and so on. For example:
--- |
--- | ```purescript
--- | type MyRecord box = { myValue :: box String }
--- |
--- | myMaybeRecord :: MyRecord Maybe
--- | myMaybeRecord = { myValue: Just "" }
--- |
--- | myArrayRecord :: MyRecord Array
--- | myArrayRecord = { myValue: [ "", "", "" ] }
--- | ```
--- |
--- | We still need to be able to produce this record, however:
--- |
--- | ```purescript
--- | myRecord :: MyRecord ???
--- | myRecord = { myValue: "" }
--- | ```
--- |
--- | How can we accomplish this? We've asserted that the `String` exists within some container,
--- | `box`, but it looks like we now need the `String` type NOT in a containing type.
--- |
--- | We can reconcile this with the `Unlifted` type synonym defined below. It's similar to the
--- | `identity` function in that it just hands back the type you provide. It's like a box that
--- | erases itself at compile-time. The correct version of `myRecord` is this:
--- |
--- | ```purescript
--- | myRecord :: MyRecord Unlifted
--- | myRecord = { myValue: "" }
--- | ```
-type Unlifted a = a
-
--- | Now we can define a shared row for various requests which manage user credentials. And the
--- | password field can be a `Maybe String` or a `String`, depending on what we need!
--- |
--- | By convention, I give row types that will later be used as records the `-Rep` suffix.
-type AuthFieldsRep box r = ( email :: Email, password :: box String | r )
-
--- | Our login and registration records both require a password field with a `String` value.
--- | However, in the `Conduit.Capability.Resource.User` module, we'll see another record using
--- | the same fields in which the password is a `Maybe String`. I encourage you to check it out
--- | to see this pattern in practice!
--- |
--- | The types below are equivalent to:
--- |
--- | ```purescript
--- | type RegisterFields = { email :: Email, password :: String, username :: Username }
--- | type LoginFields = { email :: Email, password :: String }
--- | ```
--- |
--- | Admittedly, it's not strictly necessary to share fields among the three auth types because
--- | they're so small. But this pattern is common in production applications and in libraries like
--- | `purescript-record`, `purescript-variant`, and `purescript-halogen-formless`, so it's
--- | important to be aware of it.
-type RegisterFields = { | AuthFieldsRep Unlifted (username :: Username) }
-type LoginFields = { | AuthFieldsRep Unlifted () }
+type LoginFields =
+  { email :: Email
+  , password :: String
+  }
 
 -- | This function logs a user in (if they exist), returning an auth token and the user's
 -- | minimal profile.
