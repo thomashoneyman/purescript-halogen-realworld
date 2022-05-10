f96dd0328bebf33d725f1543a3acbe44172ac438
toastal <561087+toastal@users.noreply.github.com>
2022-05-10 23:31:46 +0000

    Moved to Nix Flakes (#113)
    * Move Nix shell to flake; provide compatibility
    
    * Update Nix-related info
    
    * Remove EZ rev; kick the Flake’s tires to update

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "RealWorld spec in the PureScript Halogen framework";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    easy-purescript-nix = {
+      url = "github:justinwoo/easy-purescript-nix";
+      flake = false;
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, easy-purescript-nix, ... }@inputs:
+    let
+      name = "halogen-realworld";
+
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          easy-ps = import easy-purescript-nix { inherit pkgs; };
+        in
+        pkgs.mkShell {
+          inherit name;
+          buildInputs = (with pkgs; [
+            nodejs-16_x
+            nixpkgs-fmt
+          ]) ++ (with easy-ps; [
+            purs
+            purs-tidy
+            psa
+            spago
+            purescript-language-server
+          ]) ++ (pkgs.lib.optionals (system == "aarch64-darwin")
+            (with pkgs.darwin.apple_sdk.framework; [
+              Cocoa
+              CoreServices
+            ]));
+        });
+    };
+}
